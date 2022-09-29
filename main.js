44eee6bfab5ac042d3bcb0af233064a256ccc463
abdalmalik1996 <abd.almalik96@gmail.com>
2022-09-29 15:53:19 +0300

    js

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 let MarkAll = document.querySelector(".Mark-all");
 let NotReadable = document.querySelectorAll(".Not-readable");
 let markRead = document.querySelectorAll(".mark-read");
-console.log(markRead);
 
+let conter = document.querySelector(".conter");
+
+
+
+
+
+conter.textContent =markRead.length;
 function mark(){
-
     NotReadable.forEach(e=>{
 e.classList.remove("Not-readable");
     })
-    
     markRead.forEach(e=>{
-        e.classList.remove("mark-read")
+        e.classList.remove("mark-read");
     })
+        conter.textContent =0;
+    
 }
